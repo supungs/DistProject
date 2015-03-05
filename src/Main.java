a78ba2908bafe79e0b930c375e00c1de8456ae3a
Fahima <fathisha6@gmail.com>
2015-03-06 01:55:19 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,18 @@ public class Main {
 		
 		FileSharer sharer=new FileSharer();
 		
+		
+		
+		//		QueryMessage msg=new QueryMessage("Lord of the rings", "127.0.0.1", 10001, 4);
+		//		Sender sender=new Sender();
+		//		sender.sendUDP(msg.toString(), msg.ip_to, msg.port_to);
+		//Adding files to the node
+		int numOfFiles=4;
+		String[] fileNames={"Adventures of Tintin","Jack and Jill","Glee","Harry Potter"};
+		for(int i=0;i<numOfFiles;i++){
+		sharer.setListOfFiles(fileNames[i]);
+		}
+		
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			inFromUser.readLine();
@@ -21,11 +33,11 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
+		//Initially sending query message to the neighbors by original file
+		for(int i=0;i<sharer.neighbors.size();i++){
+		QueryMessage msg=new QueryMessage("Adventures of Tintin", sharer.neighbors.get(i).ip,sharer.neighbors.get(i).port,0);
+		sharer.sendQuery(msg);
 		System.exit(1);
-		
-//		QueryMessage msg=new QueryMessage("Lord of the rings", "127.0.0.1", 10001, 4);
-//		Sender sender=new Sender();
-//		sender.sendUDP(msg.toString(), msg.ip_to, msg.port_to);
 	}
 
 }
