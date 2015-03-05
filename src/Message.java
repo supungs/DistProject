565aefaba038a0902c2f15f98cad7022fe290272
ThamayanthyS <thamaya16@gmail.com>
2015-03-05 17:35:14 +0530

    added QueryMessage and QueryResponseMessage

--- src/Message.java
+++ src/Message.java
@@ -1,108 +1,180 @@
-
 public class Message {
 	public MsgType type;
 	public String ip_from, ip_to;
 	public int port_from, port_to;
 	public String username;
-	
-	public Message(){
-		ip_from=Config.my_ip;
-		port_from=Config.my_port;
+
+	public Message() {
+		ip_from = Config.my_ip;
+		port_from = Config.my_port;
 	}
-	
+
 	public String toString() {
 		return "";
 	}
-	public MsgType getType(){
+
+	public MsgType getType() {
 		return type;
 	}
-	
-	protected static String getLength(String msg){
-		int len=msg.length()+4;
+
+	protected static String getLength(String msg) {
+		int len = msg.length() + 4;
 		return String.format("%04d", len);
 	}
 }
 
-enum MsgType { MSG_REG, MSG_REGOK, MSG_UNROK, MSG_UNREG, MSG_JOIN, MSG_LEAVE, MSG_SEARCH, MSG_SEARCHOK, MSG_ERROR};
+enum MsgType {
+	MSG_REG, MSG_REGOK, MSG_UNROK, MSG_UNREG, MSG_JOIN, MSG_LEAVE, MSG_SEARCH, MSG_SEARCHOK, MSG_ERROR, MSG_SER, MSG_SEROK
+};
 
-class RegMessage  extends Message{
+class RegMessage extends Message {
 	String username;
-	public RegMessage(String username){
-		this.type=MsgType.MSG_REG;
-		this.username= username;
-		ip_to=Config.bootstrap_ip;
-		port_to=Config.bootstrap_port;
+
+	public RegMessage(String username) {
+		this.type = MsgType.MSG_REG;
+		this.username = username;
+		ip_to = Config.bootstrap_ip;
+		port_to = Config.bootstrap_port;
 	}
+
 	public String toString() {
-		String temp=" REG " + ip_from + " " + port_from + " "+ username;
-		return getLength(temp)+temp;
+		String temp = " REG " + ip_from + " " + port_from + " " + username;
+		return getLength(temp) + temp;
 	}
 }
 
-class RegResult  extends Message{
+class RegResult extends Message {
 	public String ip1, ip2;
-	public int  port1,  port2;
+	public int port1, port2;
 	public int num_neighbors;
-	
-	public RegResult(String msg){
-		type=MsgType.MSG_REGOK;
-		
-		String[] tokens=msg.split(" ");
-		num_neighbors=Integer.parseInt(tokens[0]);
-		if(num_neighbors>2) return;
-		if(num_neighbors>0){
-			ip1=tokens[1];
-			port1=Integer.parseInt(tokens[2]);
-		}if(num_neighbors>1){
-			ip2=tokens[4];
-			port2=Integer.parseInt(tokens[5]);
+
+	public RegResult(String msg) {
+		type = MsgType.MSG_REGOK;
+
+		String[] tokens = msg.split(" ");
+		num_neighbors = Integer.parseInt(tokens[0]);
+		if (num_neighbors > 2)
+			return;
+		if (num_neighbors > 0) {
+			ip1 = tokens[1];
+			port1 = Integer.parseInt(tokens[2]);
 		}
-		ip_from=Config.bootstrap_ip;
-		port_from=Config.bootstrap_port;
+		if (num_neighbors > 1) {
+			ip2 = tokens[4];
+			port2 = Integer.parseInt(tokens[5]);
+		}
+		ip_from = Config.bootstrap_ip;
+		port_from = Config.bootstrap_port;
 	}
 }
 
-class UnRegMessage extends Message{
+class UnRegMessage extends Message {
 	String username;
 
-	public UnRegMessage(String username){
-		this.username=username;
-		this.type=MsgType.MSG_UNREG;
-		ip_to=Config.bootstrap_ip;
-		port_to=Config.bootstrap_port;
+	public UnRegMessage(String username) {
+		this.username = username;
+		this.type = MsgType.MSG_UNREG;
+		ip_to = Config.bootstrap_ip;
+		port_to = Config.bootstrap_port;
 	}
 
-	public String toString(){
-		String temp=" UNREG " + ip_from + " " + port_from + " "+ username;
-		return getLength(temp)+temp;
+	public String toString() {
+		String temp = " UNREG " + ip_from + " " + port_from + " " + username;
+		return getLength(temp) + temp;
 	}
 }
 
-class UnRegResult extends Message{
+class UnRegResult extends Message {
 	public int status;
 
-	public UnRegResult(String msg){
-		type=MsgType.MSG_UNROK;
-		String[] tokens=msg.split(" ");
-		status=Integer.parseInt(tokens[0]);
+	public UnRegResult(String msg) {
+		type = MsgType.MSG_UNROK;
+		String[] tokens = msg.split(" ");
+		status = Integer.parseInt(tokens[0]);
 	}
 
 }
 
-class QueryFileMessage extends Message{
-	public QueryFileMessage(String fileName, String ip, String port){
-		
+/**
+ * 
+ * @author Thamayanthy
+ * @param file_name
+ */
+
+class QueryMessage extends Message {
+
+	public String file_name = "";
+	public int hops = 0;
+
+	/*
+	 * used when initiate a query for a file
+	 */
+	public QueryMessage(String file_name, String ip, int port, int hops) {
+		this.type = MsgType.MSG_SER;
+		this.file_name = file_name;
+		this.ip_from = ip;
+		this.port_from = port;
+		this.hops = hops;
+	}
+
+	/*
+	 * used when forwarding a query for a file
+	 */
+	public QueryMessage(Message queryMessage) {
+		QueryMessage message = (QueryMessage) queryMessage;
+		// message.hops=message.hops+1;
+		this.type = message.getType();
+		this.file_name = message.file_name;
+		this.ip_from = message.ip_from;
+		this.port_from = message.port_from;
+		this.hops = message.hops + 1;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see Message#toString() length SER IP port file_name hops
+	 */
+	public String toString() {
+		String temp = " SER " + ip_from + " " + port_from + " "
+				+ this.file_name + " " + this.hops;
+		return getLength(temp) + temp;
 	}
 }
 
-class ReplyQueryFileMessage extends Message{
-	
-	public ReplyQueryFileMessage(String fileName, String ip, String port){
-		
+class QueryResponseMessage extends Message {
+
+	public String file_name = "";
+	int no_file = 0;
+	String[] files;
+	int hops = 0;
+
+	/*
+	 * length SEROK no_files IP port hops filename1 filename2 ... ...
+	 */
+	public QueryResponseMessage(String ip, int port, int no_files,
+			String[] fileNames, int hops) {
+		this.type = MsgType.MSG_SEROK;
+		this.ip_from = ip;
+		this.port_from = port;
+		this.files = fileNames;
+		this.no_file = no_files;
+		this.hops = hops;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see Message#toString()
+	 */
+	public String toString() {
+		String file_names = "";
+		for (String file : files) {
+			file_names = " " + file;
+		}
+		String temp = " SEROK " + no_file + " " + ip_from + " " + port_from
+				+ " " + this.hops + " " + this.hops + file_names;
+		return getLength(temp) + temp;
 	}
 }
 
-class ForwardQueryFileMessage extends Message{
-	public ForwardQueryFileMessage(){
-	}
-}
