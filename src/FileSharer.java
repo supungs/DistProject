728c57b08b48ff42e72536cbfc16cf4995c03e85
ThamayanthyS <thamaya16@gmail.com>
2015-03-07 01:33:57 +0530

    added run method to Filesharer

--- src/FileSharer.java
+++ src/FileSharer.java
@@ -6,109 +6,150 @@ import java.util.List;
  * This is class represents the top of intermediate layer.
  * Application layer directly invoke its functionalities.
  */
-public class FileSharer {
+public class FileSharer extends Thread {
 	List<Neighbor> neighbors;
 	Messenger messenger;
-	HashMap<String,Video> fileListMap;
-	//list of files to be shared and list of downloaded files.
-	public FileSharer(){
-		neighbors=new ArrayList<Neighbor>();		
-		messenger=new Messenger();
-		fileListMap=new HashMap<String,Video>();
-		
-		RegResult response=(RegResult) messenger.sendMessage(new RegMessage(Config.my_username));
-		if(response.num_neighbors>2){
+	HashMap<String, Video> fileListMap;
+
+	// list of files to be shared and list of downloaded files.
+	public FileSharer() {
+		neighbors = new ArrayList<Neighbor>();
+		messenger = new Messenger();
+		fileListMap = new HashMap<String, Video>();
+
+		RegResult response = (RegResult) messenger.sendMessage(new RegMessage(
+				Config.my_username));
+		if (response.num_neighbors > 2) {
 			System.out.println("Server refused registration.");
 			System.exit(1);
 		}
-		if(response.num_neighbors>0){
+		if (response.num_neighbors > 0) {
 			neighbors.add(new Neighbor(response.ip1, response.port1));
-		}if(response.num_neighbors>1){
+		}
+		if (response.num_neighbors > 1) {
 			neighbors.add(new Neighbor(response.ip2, response.port2));
 		}
 
-		UnRegResult unRegResponse=(UnRegResult) messenger.sendMessage(new UnRegMessage(Config.my_username));
-		if(unRegResponse.status==0){
+		UnRegResult unRegResponse = (UnRegResult) messenger
+				.sendMessage(new UnRegMessage(Config.my_username));
+		if (unRegResponse.status == 0) {
 			System.out.println("Unregister is ok");
 		}
 	}
-	
-	
-        //Get the list of files
-	public ArrayList<String> cotainsFile(String fileName){
-		ArrayList<String> output = new ArrayList<String> ();
-			for(String s:fileListMap.keySet()){
-				if(s.toLowerCase().contains(fileName.toLowerCase())){
-					output.add(s);
+
+	/**
+	 * to automate the functions of each node,
+	 */
+	public void run() {
+		try {
+			while (true) {
+				int a = (int) Math.random() % 2;
+				switch (a) {
+				case 0:
+					for (int i = 0; i < neighbors.size(); i++) {
+						QueryMessage msg = new QueryMessage(
+								"Adventures of Tintin", neighbors.get(i).ip,
+								neighbors.get(i).port, 0);
+						sendQuery(msg);
+						System.exit(1);
+					}
+					break;
+				case 1:
+				{
+					int random=(int)Math.random()%10;
+					if(random==7){
+						//unregister
+						//register
+						//for this we should have methods for register and unregister 
+					}
+				}
+					break;
+
 				}
 			}
-			
-			return output;
-		
+		} catch (Exception e) {
+
+		}
 	}
+
 	
-	//Sending the query message
-	 public void sendQuery(Message msg){
-		 QueryResponseMessage response=(QueryResponseMessage)messenger.sendMessage(msg);
-		 if(response.no_file>=1){
-			 System.out.println("Connect to the node with minimum number of hops");
-		 }
-		 else if(response.no_file==0){
+	// Get the list of files
+	public ArrayList<String> cotainsFile(String fileName) {
+		ArrayList<String> output = new ArrayList<String>();
+		for (String s : fileListMap.keySet()) {
+			if (s.toLowerCase().contains(fileName.toLowerCase())) {
+				output.add(s);
+			}
+		}
+
+		return output;
+
+	}
+
+	// Sending the query message
+	public void sendQuery(Message msg) {
+		QueryResponseMessage response = (QueryResponseMessage) messenger
+				.sendMessage(msg);
+		if (response.no_file >= 1) {
+			System.out
+					.println("Connect to the node with minimum number of hops");
+		} else if (response.no_file == 0) {
 			System.out.println("No files found");
-		 }
-		 else if(response.no_file==9999){
+		} else if (response.no_file == 9999) {
 			System.out.println("Failure due to node unreachable");
-		 }
-		 else if(response.no_file==9998){
+		} else if (response.no_file == 9998) {
 			System.out.println("Some other error");
-		 }
-	 }
-	
-	public void sendQueryMessage(Message msg){
-		QueryMessage qmsg=(QueryMessage)msg;
-		System.out.print("sendQuery Message");
-		messenger.sender.sendUDP(qmsg.toString(),"127.0.0.1" , 10001);
+		}
 	}
-	
-	//Get the list of files
-	public ArrayList<String> containsFile(String fileName){
-		ArrayList<String> output = new ArrayList<String> ();
-		String searchwords[]= fileName.toLowerCase().split(" +");	
-		for(String file_from_list:fileListMap.keySet()){
+
+	public void sendQueryMessage(Message msg) {
+		QueryMessage qmsg = (QueryMessage) msg;
+		System.out.print("sendQuery Message");
+		messenger.sender.sendUDP(qmsg.toString(), "127.0.0.1", 10001);
+	}
+
+	// Get the list of files
+	public ArrayList<String> containsFile(String fileName) {
+		ArrayList<String> output = new ArrayList<String>();
+		String searchwords[] = fileName.toLowerCase().split(" +");
+		for (String file_from_list : fileListMap.keySet()) {
 			String lowercase_file = file_from_list.toLowerCase();
-			int available_words=0;
-			for(int i=0;i<searchwords.length;i++){
+			int available_words = 0;
+			for (int i = 0; i < searchwords.length; i++) {
 				String currentword = searchwords[i];
-				if(lowercase_file.contains(currentword+" ")||lowercase_file.contains(currentword+".")||lowercase_file.endsWith(currentword)){
+				if (lowercase_file.contains(currentword + " ")
+						|| lowercase_file.contains(currentword + ".")
+						|| lowercase_file.endsWith(currentword)) {
 					available_words++;
 				}
 			}
-				if(available_words==searchwords.length)
-					output.add(file_from_list);
-			}
-			
-			return output;
-		
+			if (available_words == searchwords.length)
+				output.add(file_from_list);
+		}
+
+		return output;
+
 	}
 
-	//Set the Index of the list of files 
-	public void setListOfFiles(String fileName){
+	// Set the Index of the list of files
+	public void setListOfFiles(String fileName) {
 		fileListMap.put(fileName, new Video());
-	}	
+	}
 }
 
-class Neighbor{
+class Neighbor {
 	public String ip;
 	public int port;
+
 	public Neighbor(String ip, int port) {
 		this.ip = ip;
 		this.port = port;
 	}
 }
 
-class Video{
+class Video {
 	public String name;
 	public int size;
-	public int duration;	
-		
+	public int duration;
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
 
 	public static void main(String[] args) {
-		Config.my_username="useral1";
-		Config.bootstrap_ip="127.0.0.1";
-		Config.bootstrap_port=9999;
-		Config.my_ip="127.0.0.1";
-		Config.my_port=10001;
-		
-		FileSharer sharer=new FileSharer();		
-		
-		
-		//		QueryMessage msg=new QueryMessage("Lord of the rings", "127.0.0.1", 10001, 4);
-		//		Sender sender=new Sender();
-		//		sender.sendUDP(msg.toString(), msg.ip_to, msg.port_to);
-		//Adding files to the node
-		int numOfFiles=4;
-		String[] fileNames={"Adventures of Tintin","Jack and Jill","Glee","Harry Potter"};
-		for(int i=0;i<numOfFiles;i++){
-		sharer.setListOfFiles(fileNames[i]);
+		Config.my_username = "useral1";
+		Config.bootstrap_ip = "127.0.0.1";
+		Config.bootstrap_port = 9999;
+		Config.my_ip = "127.0.0.1";
+		Config.my_port = 10001;
+
+		FileSharer sharer = new FileSharer();
+
+		// QueryMessage msg=new QueryMessage("Lord of the rings", "127.0.0.1",
+		// 10001, 4);
+		// Sender sender=new Sender();
+		// sender.sendUDP(msg.toString(), msg.ip_to, msg.port_to);
+		// Adding files to the node
+		int numOfFiles = 4;
+		String[] fileNames = { "Adventures of Tintin", "Jack and Jill", "Glee",
+				"Harry Potter" };
+		for (int i = 0; i < numOfFiles; i++) {
+			sharer.setListOfFiles(fileNames[i]);
 		}
-		
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
+				System.in));
 		try {
 			inFromUser.readLine();
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
-		} 
-		//Initially sending query message to the neighbors by original file
-		for(int i=0;i<sharer.neighbors.size();i++){
-		QueryMessage msg=new QueryMessage("Adventures of Tintin", sharer.neighbors.get(i).ip,sharer.neighbors.get(i).port,0);
-		sharer.sendQuery(msg);
-		System.exit(1);
-	}
+		}
+		// Initially sending query message to the neighbors by original file
+		for (int i = 0; i < sharer.neighbors.size(); i++) {
+			QueryMessage msg = new QueryMessage("Adventures of Tintin",
+					sharer.neighbors.get(i).ip, sharer.neighbors.get(i).port, 0);
+			sharer.sendQuery(msg);
+			System.exit(1);
+		}
 	}
 }
